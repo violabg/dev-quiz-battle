0bd7203c3b66a464c167acb97074e353f923bec5
Paolo Bernardini <paolo.bernardini@spindox.it>
2025-05-06 17:54:31 +0200

    Add favicon images and site manifest
    - Added favicon-16x16.png and favicon-32x32.png for improved browser icon display.
    - Introduced favicon.ico for legacy support across various platforms.
    - Created site.webmanifest for PWA support, including icons and color settings.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,25 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
